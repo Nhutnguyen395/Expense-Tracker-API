c4315bc55cb90d2a7930b97c0465970a9fbf4b8f
Nhutnguyen395 <nhutnguyen395@gmail.com>
2025-09-15 21:08:39 -0700

    added authController for signup and login

---
+++ controllers/authController.js
@@ -0,0 +1,47 @@
+import User from '../models/User';
+import jwt from 'jasonwebtoken';
+
+export const signup = async(req, res) => {
+  try {
+    // 1. Get username and password from the request body
+    const {username, password} = req.body;
+
+    // 2. Create a new user instance using the User Model
+    const user = new User({username, password});
+
+    // 3. Save the instance to the database
+    await user.save();
+
+    // 4. Send back a succes response
+    res.stats(201).send({message: 'User created successfully'});
+  } catch (error) {
+    res.status(400).send({error: 'Error creating user'});
+  }
+};
+
+export const login = async(req, res) => {
+  try {
+    // 1. get credentials from the request
+    const {username, password} = req.body;
+    
+    // 2. Find the user and their password
+    const user = await User.findOne({username});
+
+    // 3. Verify the user and their password
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).send({error: 'Invalid credentials'});
+    }
+
+    // 4. If credentials are valid, create JWT
+    const token = jwt.sign(
+      {userId: user.id},
+      process.env.JWT_SECRET,
+      {expiresIn: '1h'}
+    );
+
+    // 5. Send the token back to the user
+    res.send({token});
+  } catch (error){
+    res.status(400).send({error: 'Error logging in'});
+  }
+};
